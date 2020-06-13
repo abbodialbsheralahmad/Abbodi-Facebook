dbc7c9da4371324d230ae1fa1c31465c7a06e3da
abbodialbsheralahmad <62264761+abbodialbsheralahmad@users.noreply.github.com>
2020-06-13 22:45:36 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,39 @@
+
+
+
+#          ABBODI ALBSHER - FaceBook
+
+
+#            install  for Linux
+
+apt update && apt upgrade -y
+
+
+apt install python -y
+
+
+apt install python2 -y
+
+
+apt install pip -y
+
+
+apt install pip2
+
+
+pip upgrade
+
+
+pip2 install requests
+
+
+pip2 install mechanize
+
+
+chmod +x *
+
+
+echo ""
+
+
+echo " [+] Done ... "
